96a3a9eb17ce24fd6ddbb6533b0c62b66ee99ba9
pishoy <you@example.com>
2019-01-20 15:07:43 +0200

    farmlist

--- list_farm.py
+++ list_farm.py
@@ -1,20 +1,54 @@
 from jumpscale import j
+import pandas as pd
 
 # declaring list of store data
+nodes = []
+nodes_os = []
+last_updated = []
+
+# example
+# s = farmers()
+# s.list()
+# s.check_farm_existing('bishoy_farm')
 
 class farmers:
-    def list(self):
+    def __init__(self):
         self.capacity = j.clients.threefold_directory.get(interactive=False)
         mylist, response = self.capacity.api.ListFarmers()
         self.farm_names = []
         for obj in mylist:
             self.farm_names.append(obj.name)
-        return self.farm_names
+        #return self.farm_names
+
 
     def check_farm_existing (self,myfarm):
+            self.myfarm = myfarm
+            try:
+                self.capacity.api.GetFarmer(self.myfarm)
+                print ('the farm {} is exist ....'.format(self.myfarm))
+            except:
+                print ('the farm {} does not exist on our grid, check may no node are added to it '.format(self.myfarm))
+
+# get nodes using convert dict to list then use pandas class
+    def check_farm_nodes (self,myfarm):
         self.myfarm = myfarm
-        for f in self.farm_names:
-            if f == self.myfarm:
-                print ('the farm name {} is exist ....'.format(self.myfarm))
-            #else:
-            #    print ('the farm name {} does not exist on our grid, check may no node are added to it '.format(self.myfarm))
+        nodes =[]
+        nodes_os = []
+        last_updated = []
+        self.resp = self.capacity.api.ListCapacity(query_params={'farmer': self.myfarm})[1]
+        self.nodes = self.resp.json()
+        for node in self.nodes:
+            node_id = node['node_id']
+            nodes.append(node_id)
+            os_version = node['os_version']
+            nodes_os.append(os_version)
+            updated = node['updated']
+            last_updated.append(updated)
+        import ipdb;ipdb.set_trace()
+        import pandas as pd
+        table = pd.DataFrame({'node_id': nodes,
+                              'os_version':nodes_os,
+                              'last_updated' : last_updated})
+        print ('Total number of nodes in this farm is {}'.format(len(nodes)))
+        print (table[1:])
+
