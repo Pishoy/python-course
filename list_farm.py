b598d633576ce6a9a82e1ab91c7cda361222db77
Bishoy <abdelmab@codescalers.com>
2019-01-22 18:09:54 +0200

    testing

--- list_farm.py
+++ list_farm.py
@@ -3,13 +3,13 @@
 from jumpscale import j
 import pandas as pd
 import click
+from urllib import error
 
-
-# example
-# s = farmers()
-# s.list()
-# s.check_farm_existing('bishoy_farm')
-# s.check_farm_nodes('bishoy_farm')
+#import ipdb;ipdb.set_trace()
+# declare my variables
+nodes =[]
+nodes_os = []
+last_updated = []
 
 def check_farm_existing(myfarm):
     try:
@@ -19,30 +19,35 @@ def check_farm_existing(myfarm):
     except:
         print('the farm {} does not exist on our grid, check may no node are added to it '.format(self.myfarm))
 
-click.command()
-click.option('--farm', '-f', help='put your farm name ',required = True)
-# get nodes using convert dict to list then use pandas class
-def check_farm_nodes (myfarm):
-    nodes =[]
-    nodes_os = []
-    last_updated = []
-    capacity = j.clients.threefold_directory.get(interactive=False)
-    resp = capacity.api.ListCapacity(query_params={'farmer': myfarm})[1]
-    nodes = self.resp.json()
-    for node in nodes:
-        node_id = node['node_id']
-        nodes.append(node_id)
-        os_version = node['os_version']
-        nodes_os.append(os_version)
-        updated = node['updated']
-        last_updated.append(updated)
-    #import ipdb;ipdb.set_trace()
-    import pandas as pd
-    table = pd.DataFrame({'node_id': nodes,
-                          'os_version':nodes_os,
-                          'last_updated' : last_updated})
-    print ('Total number of nodes in this farm is {}'.format(len(nodes)))
-    print (table[1:])
 
-if __name__ == '__main__':
-    check_farm_nodes()
+@click.command()
+@click.option('--myfarm', '-f', help='put your farm name ', required=True)
+
+# get nodes using convert dict to list then use pandas class
+def main (myfarm):
+    # check existing first then list nodes
+    try:
+        capacity = j.clients.threefold_directory.get(interactive=False)
+        capacity.api.GetFarmer(myfarm)
+        resp = capacity.api.ListCapacity(query_params={'farmer': myfarm})[1]
+        mynodes = resp.json()
+        for node in mynodes:
+            node_id = node['node_id']
+            nodes.append(node_id)
+            os_version = node['os_version']
+            nodes_os.append(os_version)
+            updated = node['updated']
+            last_updated.append(updated)
+        table = pd.DataFrame({'node_id': nodes,
+                              'os_version':nodes_os,
+                              'last_updated' : last_updated})
+        print ('Total number of nodes in this farm is {}'.format(len(nodes)))
+        print (table[1:])
+    except HTTPError, err:
+        if err.code == 404:
+            err.meg = ' farmer name %s does not exist on our grid %s' % (myfarm,err.meg)
+        raise
+
+
+if __name__ == "__main__":
+    main()
